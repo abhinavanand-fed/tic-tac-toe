454e6326bf26657d92a3fad0a7da0f8005789aba
Abhinav Anand <110715459+abhinavanand-fed@users.noreply.github.com>
2022-12-15 20:08:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 //For implementing bootstrap(Line number 2-5)
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import $ from 'jquery';
-import Popper from 'popper.js';
+
 
 import React from 'react';
 import ReactDOM from 'react-dom';
